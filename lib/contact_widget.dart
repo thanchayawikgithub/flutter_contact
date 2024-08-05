c7e7310de59e6be4b87c73af3c28d5920776e4c4
thanchayawik <thanchayawik@gmail.com>
2024-08-05 17:38:04 +0700

    update contact detail

--- lib/contact_widget.dart
+++ lib/contact_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_contact/contact.dart';
 
 class ContactWidget extends StatefulWidget {
   const ContactWidget({Key? key}) : super(key: key);
@@ -8,12 +9,56 @@ class ContactWidget extends StatefulWidget {
 }
 
 class _ContactWidgetState extends State<ContactWidget> {
+  List<Contact> contacts = [
+    Contact(
+        id: 1,
+        name: 'Contact1',
+        phone: "0999999999",
+        email: 'contact1@gmail.com'),
+    Contact(
+        id: 2,
+        name: 'Contact2',
+        phone: "0999999992",
+        email: 'contact2@gmail.com'),
+    Contact(
+        id: 3,
+        name: 'Contact3',
+        phone: "0999999993",
+        email: 'contact3@gmail.com'),
+    Contact(
+        id: 4,
+        name: 'Contact4',
+        phone: "0999999994",
+        email: 'contact4@gmail.com'),
+    Contact(
+        id: 5,
+        name: 'Contact5',
+        phone: "0999999995",
+        email: 'contact5@gmail.com')
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("My Contact")),
-      body: Center(
-        child: Text("My Contact"),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(child: Text(contacts[index].name[0])),
+            title: Text(contacts[index].name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(contacts[index].phone),
+                Text(contacts[index].email)
+              ],
+            ),
+          );
+        },
+        itemCount: contacts.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
